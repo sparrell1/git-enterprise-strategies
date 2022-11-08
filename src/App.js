953b75e996d23dbcd496a5d21475f4a655976164
samilieberman <samilieberman1@gmail.com>
2022-11-07 19:50:35 -0500

    add contributing.md
    Add new text and gator image
    
    Updated name to my name Sean Parrell in App.js
    
    1.0.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
+import gatorLogo from './gator.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< HEAD
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
+<<<<<<< HEAD
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +19,13 @@ function App() {
         >
           Learn React
         </a>
+=======
+        <img src={gatorLogo} className="App-logo" alt="logo" />
+        <p> Hello World! </p>
+        <p> My name is Sean Parrell and this is my project</p>
+>>>>>>> 0a9de42 (Add new text and gator image)
+=======
+>>>>>>> 6cb53b9 (add hello world message)
       </header>
     </div>
   );
