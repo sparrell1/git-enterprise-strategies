859f421c967a6be6152fa80c0d1e8b23060588ef
Sean <sean@parrell.com>
2024-09-08 17:10:41 -0400

    Update App.js before version bump

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+          <header className="App-header">
+           <img src={gatorLogo} className="App-logo" alt="logo" />
+           <p>My name is Sean Parrell and this is my project</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is our team project.
