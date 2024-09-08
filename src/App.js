e42cb1048f4e8c34ed9df39504ae5335b388c398
sparrell1 <163921853+sparrell1@users.noreply.github.com>
2024-09-08 18:25:21 -0400

    Revert "add contributing.md"

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import gatorLogo from './gator.gif';
 import './App.css';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={gatorLogo} className="App-logo" alt="logo" />
-                <p>
-                    Hello World!
-                </p>
-                <p>
-                    My name is Sean Parrell and this is my project
-                </p>
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World! This is our team project.
+        </p>	
+        242861e (added new project text)
+      </header>
+    </div>
+  );
 }
 
 export default App;
